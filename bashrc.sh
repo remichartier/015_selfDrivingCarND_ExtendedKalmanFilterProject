7508e4fd4d0077dd5f208a24ca7d9a468bf8d80c
remichartier <remipr.chartier@gmail.com>
2020-12-24 07:53:28 +0000

    backup original .cpp files in src/001_archiveOlderVersions folder

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,3 @@
 git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
 git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
-<<<<<<< HEAD
-=======
-
->>>>>>> d6dea228e7c454170b29eb0b0606dc467e851b9e
