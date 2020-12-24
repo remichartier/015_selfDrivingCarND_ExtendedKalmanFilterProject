bd30fba30991e9907e3a747ba8218ffef8a18fa1
remichartier <remipr.chartier@gmail.com>
2020-12-24 07:45:03 +0000

    push all initial files of the project

---
+++ bashrc.sh
@@ -0,0 +1,3 @@
+git config --global user.email "remipr.chartier@gmail.com"
+git config --global user.name "remichartier"
+git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
