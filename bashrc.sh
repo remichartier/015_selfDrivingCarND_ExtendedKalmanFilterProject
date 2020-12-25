8a09e59fc0717a5fd1b56f8ae3070087b19bdb32
remichartier <remipr.chartier@gmail.com>
2020-12-25 05:10:13 +0000

    add setup to git ssh-agent

--- bashrc.sh
+++ bashrc.sh
@@ -2,3 +2,5 @@ git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
 # git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
 git remote set-url origin "git@github.com:remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
+eval "$(ssh-agent -s)"
+ssh-add .ssh/20201224_1219_github_ed25519
