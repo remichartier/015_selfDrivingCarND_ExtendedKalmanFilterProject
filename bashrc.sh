16af85815291e016376a54c40b858afc60d7a73b
remichartier <remipr.chartier@gmail.com>
2020-12-25 03:26:09 +0000

    modify bashrc to use ssh

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,4 @@
 git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
-git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
+# git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
+git remote set-url origin "git@github.com:remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
